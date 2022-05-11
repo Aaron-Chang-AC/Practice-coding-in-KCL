8076ecb9029c635adf8eefec058f1b3b720e7269
rogerchenrc <rogerchen15935717@gmail.com>
2022-05-11 22:59:26 +0100

    Update week10.py

--- PNN/week10.py
+++ PNN/week10.py
@@ -54,7 +54,7 @@ def k_means(datapoint, c, cluster_point=None, randomized=False):
             assigned_new_class_list[:] = []
         else:
             if assigned_class_list == assigned_new_class_list:
-                print(f"Proccess Done in Round {round}")
+                print(f"Proccess Done in Round {round}\n\n")
                 break
             else:
                 assigned_class_list = assigned_new_class_list.copy()
@@ -72,7 +72,10 @@ def k_means(datapoint, c, cluster_point=None, randomized=False):
         new_coordinates = np.zeros((num_cluster, len(data[0])))
         new_coordinates_cnt = np.zeros(num_cluster)
         round += 1
-
+    for i in range(len(assigned_class_list)):
+        print(f"X{i + 1} belongs to class {assigned_class_list[i]+1}")
+    for i in range(len(clusters)):
+        print(f"cluster {i + 1}: {clusters[i]}")
     return clusters
 
 def PCA(S, dimension, new_samples_to_be_classified):
@@ -80,11 +83,15 @@ def PCA(S, dimension, new_samples_to_be_classified):
     X = S.copy().T
     print(f"original dataset(each column is a sample):\n{X}\n")
     m = X.mean(axis=1)
+    print(f"mean:\n{m.reshape(-1,1)}\n")
     Xi_m= X-m.reshape(-1,1)
     covariance_matrix=(Xi_m @ Xi_m.T)/n
     print(f"covariance_matrix:\n{covariance_matrix}\n")
     V, D, VT = np.linalg.svd(covariance_matrix, full_matrices=True)
-    D = np.diag(D)
+    idx = (-D).argsort()
+    D = np.diag(D[idx])
+    V = V[:,idx]
+    VT = V.T
     print(f"V:\n{V}\n")
     print(f"D:\n{D}\n")
     print(f"VT:\n{VT}\n")
@@ -103,10 +110,8 @@ def competitive_learning_algorithm(S,iterations,initial_centers,chosen_order,new
     '''
      chosen_order: A np array which should start from 0, and it contains the indices that we
      can use to select the samples from dataset X
-
      new_data_to_be_classfied: A np array which contains the new given datapoints, if not given
      in the question, just manually give a legal datapoint
-
     '''
     n = len(S)
     n_centers = len(initial_centers)
@@ -120,7 +125,7 @@ def competitive_learning_algorithm(S,iterations,initial_centers,chosen_order,new
         m = np.append(m, initial_centers.copy(), axis=1)
         target = np.append(target, new_data_to_be_classfied.copy(), axis=1)
         for i in range(n):
-            X[i]=X[i]/np.linalg.norm(X[i])
+            X[i] = X[i] / np.linalg.norm(X[i])
         for i in range(n_centers):
             m[i] = m[i] / np.linalg.norm(m[i])
         for i in range(n_new_samples):
@@ -136,45 +141,78 @@ def competitive_learning_algorithm(S,iterations,initial_centers,chosen_order,new
     print(f"original dataset(each column is a sample):\n{X}\n")
     print(f"initial centers(each column is a center):\n{m}\n")
 
-    for i in range(iterations):
-        print(f"-------------Iteration {i+1}-------------\n")
-        sample_x = X[:,chosen_order[i]]
-        dist=np.zeros(n_centers)
+    if normalization_flag:
+        for i in range(iterations):
+            print(f"-------------Iteration {i + 1}-------------\n")
+            sample_x = X[:, chosen_order[i]]
+            dist = np.zeros(n_centers)
 
-        for j in range(n_centers):
-            dist[j] = np.linalg.norm(sample_x - m[:,j])
-            print(f"distance to center {j+1}: {dist[j]}\n")
+            for j in range(n_centers):
+                dist[j] = np.dot(sample_x.flatten(), m[:, j].flatten())
+                print(f"dot product of weight {j + 1}: {dist[j]}\n")
 
-        update_index = np.argmin(dist)
-        m[:,update_index] = m[:,update_index] + learning_rate * (sample_x-m[:,update_index])
-        print(f"updated center {update_index+1}:\n{m[:,update_index]}\n")
+            update_index = np.argmax(dist)
+            m[:, update_index] = m[:, update_index] + learning_rate * sample_x
+            print(f"updated weight {update_index + 1}:\n{m[:, update_index]}\n")
+            m[:, update_index] = m[:, update_index] / np.linalg.norm(m[:, update_index])
+            print(f"normalize weight {update_index + 1}:\n{m[:, update_index]}\n")
 
-    print(f"all updated centers:(each column is a center)\n{m}\n")
+        print(f"all updated weights:(each column is a weight)\n{m}\n")
 
-    print(f"assigned classes to all original samples:")
-    for i in range(n):
-        dist = np.zeros(n_centers)
-        sample_x = X[:,i]
-        for j in range(n_centers):
-            dist[j] = np.linalg.norm(sample_x - m[:,j])
-        print(f"sample {i+1}: class {np.argmin(dist)+1}")
+        print(f"assigned classes to all original samples:")
+        for i in range(n):
+            dist = np.zeros(n_centers)
+            sample_x = X[:, i]
+            for j in range(n_centers):
+                dist[j] = np.dot(sample_x.flatten(), m[:, j].flatten())
+            print(f"sample {i + 1}: class {np.argmax(dist) + 1}")
 
-    print(f"\n\nassigned classes to all new given samples:")
-    for i in range(n_new_samples):
-        dist = np.zeros(n_centers)
-        sample_x = target[:,i]
-        for j in range(n_centers):
-            dist[j] = np.linalg.norm(sample_x - m[:,j])
-        print(f"sample {i+1}: class {np.argmin(dist)+1}")
+        print(f"\n\nassigned classes to all new given samples:")
+        for i in range(n_new_samples):
+            dist = np.zeros(n_centers)
+            sample_x = target[:, i]
+            for j in range(n_centers):
+                dist[j] = np.dot(sample_x.flatten(), m[:, j].flatten())
+            print(f"sample {i + 1}: class {np.argmax(dist) + 1}")
+
+    else:
+        for i in range(iterations):
+            print(f"-------------Iteration {i + 1}-------------\n")
+            sample_x = X[:, chosen_order[i]]
+            dist = np.zeros(n_centers)
+
+            for j in range(n_centers):
+                dist[j] = np.linalg.norm(sample_x - m[:, j])
+                print(f"distance to center {j + 1}: {dist[j]}\n")
+
+            update_index = np.argmin(dist)
+            m[:, update_index] = m[:, update_index] + learning_rate * (sample_x - m[:, update_index])
+            print(f"updated center {update_index + 1}:\n{m[:, update_index]}\n")
+
+        print(f"all updated centers:(each column is a center)\n{m}\n")
+
+        print(f"assigned classes to all original samples:")
+        for i in range(n):
+            dist = np.zeros(n_centers)
+            sample_x = X[:, i]
+            for j in range(n_centers):
+                dist[j] = np.linalg.norm(sample_x - m[:, j])
+            print(f"sample {i + 1}: class {np.argmin(dist) + 1}")
+
+        print(f"\n\nassigned classes to all new given samples:")
+        for i in range(n_new_samples):
+            dist = np.zeros(n_centers)
+            sample_x = target[:, i]
+            for j in range(n_centers):
+                dist[j] = np.linalg.norm(sample_x - m[:, j])
+            print(f"sample {i + 1}: class {np.argmin(dist) + 1}")
 
 def basic_leader_follower_algorithm(S,iterations,initial_centers,chosen_order,new_data_to_be_classfied,learning_rate,theta,normalization_flag):
     '''
      chosen_order: A np array which should start from 0, and it contains the indices that we
      can use to select the samples from dataset X
-
      new_data_to_be_classfied: A np array which contains the new given datapoints, if not given
      in the question, just manually give a legal datapoint
-
     '''
     n = len(S)
     n_centers = len(initial_centers)
@@ -203,49 +241,90 @@ def basic_leader_follower_algorithm(S,iterations,initial_centers,chosen_order,ne
 
     print(f"original dataset(each column is a sample):\n{X}\n")
     print(f"initial centers(each column is a center):\n{m}\n")
+    if normalization_flag:
+        for i in range(iterations):
+            print(f"-------------Iteration {i + 1}-------------\n")
+            sample_x = X[:, chosen_order[i]]
+            dist = np.zeros(n_centers)
 
-    for i in range(iterations):
-        print(f"-------------Iteration {i+1}-------------\n")
-        sample_x = X[:,chosen_order[i]]
-        dist=np.zeros(n_centers)
+            for j in range(n_centers):
+                dist[j] = np.dot(sample_x.flatten(), m[:, j].flatten())
+                print(f"dot product of weight {j + 1}: {dist[j]}\n")
 
-        for j in range(n_centers):
-            dist[j] = np.linalg.norm(sample_x - m[:,j])
-            print(f"distance to center {j+1}: {dist[j]}\n")
+            best_weight_idx = np.argmax(dist)
+            temp = np.linalg.norm(sample_x - m[:, best_weight_idx])
+            print(f"distance of the best cluster to the sample: {temp}")
+            if temp < theta:
+                m[:, best_weight_idx] = m[:, best_weight_idx] + learning_rate * sample_x
+                print(f"updated center {best_weight_idx + 1}:\n{m[:, best_weight_idx]}\n")
+                m[:, best_weight_idx] = m[:, best_weight_idx] / np.linalg.norm(m[:, best_weight_idx])
+                print(f"normalize center {best_weight_idx + 1}:\n{m[:, best_weight_idx]}\n")
+            else:
+                n_centers += 1
+                print(f"original closest center {best_weight_idx + 1}:\n{m[:, best_weight_idx]}\n")
+                m = np.append(m, np.asarray([sample_x.copy()]).T, axis=1)
+                m[:, n_centers - 1] = m[:, n_centers - 1] / np.linalg.norm(m[:, n_centers - 1])
+                print(f"new center {n_centers}:\n{m[:, n_centers - 1]}\n")
 
-        if np.min(dist)<theta:
-            update_index = np.argmin(dist)
-            m[:, update_index] = m[:, update_index] + learning_rate * (sample_x - m[:, update_index])
-            print(f"updated center {update_index + 1}:\n{m[:, update_index]}\n")
-        else:
-            n_centers+=1
-            min_dist_index= np.argmin(dist)
-            print(f"original closest center {min_dist_index + 1}:\n{m[:, min_dist_index]}\n")
-            m = np.append(m, np.asarray([sample_x.copy()]).T, axis=1)
-            print(f"new center {n_centers}:\n{m[:, n_centers-1]}\n")
+        print(f"all updated centers:(each column is a center)\n{m}\n")
 
-    print(f"all updated centers:(each column is a center)\n{m}\n")
+        print(f"assigned classes to all original samples:")
+        for i in range(n):
+            dist = np.zeros(n_centers)
+            sample_x = X[:, i]
+            for j in range(n_centers):
+                dist[j] = np.dot(sample_x.flatten(), m[:, j].flatten())
+            print(f"sample {i + 1}: class {np.argmax(dist) + 1} weight:{np.max(dist)}")
 
-    print(f"assigned classes to all original samples:")
-    for i in range(n):
-        dist = np.zeros(n_centers)
-        sample_x = X[:,i]
-        for j in range(n_centers):
-            dist[j] = np.linalg.norm(sample_x - m[:,j])
-        print(f"sample {i+1}: class {np.argmin(dist)+1} distance:{np.min(dist)}")
+        print(f"\n\nassigned classes to all new given samples:")
+        for i in range(n_new_samples):
+            dist = np.zeros(n_centers)
+            sample_x = target[:, i]
+            for j in range(n_centers):
+                dist[j] = np.dot(sample_x.flatten(), m[:, j].flatten())
+            print(f"sample {i + 1}: class {np.argmax(dist) + 1} weight:{np.max(dist)}")
+    else:
+        for i in range(iterations):
+            print(f"-------------Iteration {i + 1}-------------\n")
+            sample_x = X[:, chosen_order[i]]
+            dist = np.zeros(n_centers)
 
-    print(f"\n\nassigned classes to all new given samples:")
-    for i in range(n_new_samples):
-        dist = np.zeros(n_centers)
-        sample_x = target[:,i]
-        for j in range(n_centers):
-            dist[j] = np.linalg.norm(sample_x - m[:,j])
-        print(f"sample {i+1}: class {np.argmin(dist)+1} distance:{np.min(dist)}")
+            for j in range(n_centers):
+                dist[j] = np.linalg.norm(sample_x - m[:, j])
+                print(f"distance to center {j + 1}: {dist[j]}\n")
+
+            if np.min(dist) < theta:
+                update_index = np.argmin(dist)
+                m[:, update_index] = m[:, update_index] + learning_rate * (sample_x - m[:, update_index])
+                print(f"updated center {update_index + 1}:\n{m[:, update_index]}\n")
+            else:
+                n_centers += 1
+                min_dist_index = np.argmin(dist)
+                print(f"original closest center {min_dist_index + 1}:\n{m[:, min_dist_index]}\n")
+                m = np.append(m, np.asarray([sample_x.copy()]).T, axis=1)
+                print(f"new center {n_centers}:\n{m[:, n_centers - 1]}\n")
+
+        print(f"all updated centers:(each column is a center)\n{m}\n")
+
+        print(f"assigned classes to all original samples:")
+        for i in range(n):
+            dist = np.zeros(n_centers)
+            sample_x = X[:, i]
+            for j in range(n_centers):
+                dist[j] = np.linalg.norm(sample_x - m[:, j])
+            print(f"sample {i + 1}: class {np.argmin(dist) + 1} distance:{np.min(dist)}")
+
+        print(f"\n\nassigned classes to all new given samples:")
+        for i in range(n_new_samples):
+            dist = np.zeros(n_centers)
+            sample_x = target[:, i]
+            for j in range(n_centers):
+                dist[j] = np.linalg.norm(sample_x - m[:, j])
+            print(f"sample {i + 1}: class {np.argmin(dist) + 1} distance:{np.min(dist)}")
 
 
 def fuzzyKMeans(dataset, numCluster, initial_membership, b, criteria):
     """
-
     :param dataset:
     :param numCluster:
     :param initial_membership: u (greek letter mu)
@@ -262,8 +341,8 @@ def fuzzyKMeans(dataset, numCluster, initial_membership, b, criteria):
         new_pair[:] = []
 
     normalized_membership = np.asarray(normalized_membership.copy())
-    print(f"Normalized Membership is: {normalized_membership}")
-    print(normalized_membership.T)
+    print(f"Normalized Membership is:\n{normalized_membership}")
+    print(f"Transpose of Normalized Membership is:\n{normalized_membership.T}\n\n")
 
     #cluster point
     cluster_point = np.zeros((numCluster,len(dataset[0])))
@@ -279,8 +358,9 @@ def fuzzyKMeans(dataset, numCluster, initial_membership, b, criteria):
                 cluster_point[i] += np.multiply(normalized_membership.T[i][j]**b, dataset[j])
             cluster_point[i] /= np.sum(normalized_membership.T[i]**b)
 
-        print(cluster_point)
-
+        for i in range(numCluster):
+            print(f"updated m{i+1}: {cluster_point[i]}")
+        print("\n")
         TEMP = normalized_membership.copy().T
         for i in range(numCluster): # number of clusters
             for j in range(len(dataset)):
@@ -289,7 +369,7 @@ def fuzzyKMeans(dataset, numCluster, initial_membership, b, criteria):
                 TEMP[i,j] = dist
         TEMP = TEMP / np.sum(TEMP, axis=0)
         normalized_membership = TEMP.copy().T
-        print(f"updated normalized_membership:\n{normalized_membership.T}")
+        print(f"updated normalized_membership:\n{normalized_membership.T}\n")
 
         if round == 1:
             old_cluster_point = cluster_point.copy()
@@ -306,10 +386,6 @@ def fuzzyKMeans(dataset, numCluster, initial_membership, b, criteria):
             else:
                 old_cluster_point = cluster_point.copy()
         round+=1
-
-# dataset = np.asarray([[-1, 3], [1, 4], [0, 5], [4, -1], [3, 0], [5, 1]])
-# u = [[1, 0], [0.5, 0.5], [0.5, 0.5], [0.5, 0.5], [0.5, 0.5], [0, 1]]
-# fuzzyKMeans(dataset=dataset, numCluster=2, initial_membership=u, b=2, criteria=0.5)
 
 
 from sklearn.metrics.pairwise import euclidean_distances
@@ -436,15 +512,6 @@ def agglomerative_clustering(dataset, numCluster, link_type=None):
 
         return d_average, df[0].to_numpy()
 
-
-
-# hdataset = np.asarray([[-1, 3], [1, 2], [0, 1], [4, 0], [5, 4], [3, 2]])
-#
-# d , df = agglomerative_clustering(dataset= hdataset, numCluster= 3, link_type="single")
-# print("=====Return value ======")
-# print(d)
-# print(df)
-
 def euclidean_distance(cluster, datapoint):
     """
     For 2 or more dimensions
@@ -469,9 +536,27 @@ def abs_distance(cluster, datapoint):
         result.append(np.linalg.norm(cluster[i] - datapoint, ord=1))
     return result
 
-# datapoint = np.asarray([[-1, 3],[1, 4],[0, 5], [4, -1],[3, 0], [5, 1]])
+# EXECUTION #############################################################
 
-# PCA
+# for k_means
+# Note that each "row" is a sample, c is the number of clusters
+# cluster_point is the initial clusters
+# datapoint = np.asarray([
+#     [-1, 3],
+#     [1, 4],
+#     [0, 5],
+#     [4, -1],
+#     [3, 0],
+#     [5, 1]
+# ])
+# cluster_point=np.asarray([
+#     [-1, 3],
+#     [5, 1]
+# ])
+# k_means(datapoint=datapoint, c=2, cluster_point=cluster_point, randomized=False)
+
+# PCA different from that of week7
+# new_samples_to_be_classified need to be subtracted by the mean manually first!!
 # S = np.asarray([
 #     [4,2,2],
 #     [0,-2,2],
@@ -481,13 +566,12 @@ def abs_distance(cluster, datapoint):
 # new_samples_to_be_classified = np.asarray([
 #         [3,-2,5]
 # ])
-
-# print(k_means(datapoint, 2, [[-1, 3], [5, 1]], randomized=False))
-# print(PCA(S,dimension=1,new_samples_to_be_classified=new_samples_to_be_classified))
-# print(euclidean_distance(np.asarray([[-2.8284, 0],[2.8284, 0]]), np.asarray([-0.7071, -3.5355])))
+#
+# print(PCA(S,dimension=2,new_samples_to_be_classified=new_samples_to_be_classified))
 
 
 # competitive_learning_algorithm
+# Note that each integer in chosen_order is >= 0
 # S = np.asarray([
 #     [-1,3],
 #     [1,4],
@@ -507,7 +591,12 @@ def abs_distance(cluster, datapoint):
 #     [-0.2,2.8],
 #     [0,1.5]
 # ])
-# competitive_learning_algorithm(S,5,initial_centers,chosen_order,new_data_to_be_classfied,learning_rate=0.1,normalization_flag=False)
+# competitive_learning_algorithm(S,iterations=5,initial_centers= initial_centers,
+#                                chosen_order=chosen_order,
+#                                new_data_to_be_classfied=new_data_to_be_classfied,
+#                                learning_rate=0.1,
+#                                normalization_flag=False
+#                                )
 
 
 # basic_leader_follower_algorithm
@@ -528,4 +617,46 @@ def abs_distance(cluster, datapoint):
 #     [-0.2,2.8],
 #     [0,1.5]
 # ])
-# basic_leader_follower_algorithm(S,5,initial_centers,chosen_order,new_data_to_be_classfied,learning_rate=0.5,theta=3.0,normalization_flag=False)
+# basic_leader_follower_algorithm(S,iterations=5,initial_centers=initial_centers,
+#                                 chosen_order=chosen_order,
+#                                 new_data_to_be_classfied=new_data_to_be_classfied,
+#                                 learning_rate=0.5,
+#                                 theta=3.0,
+#                                 normalization_flag=False
+#                                 )
+
+# for fuzzyKMeans
+# dataset = np.asarray([
+#     [-1, 3],
+#     [1, 4],
+#     [0, 5],
+#     [4, -1],
+#     [3, 0],
+#     [5, 1]
+# ])
+# u = np.asarray([
+#     [1, 0],
+#     [0.5, 0.5],
+#     [0.5, 0.5],
+#     [0.5, 0.5],
+#     [0.5, 0.5],
+#     [0, 1]
+# ])
+# fuzzyKMeans(dataset=dataset, numCluster=2, initial_membership=u, b=2, criteria=0.5)
+
+# for agglomerative_clustering
+hdataset = np.asarray([
+    [-1, 3],
+    [1, 2],
+    [0, 1],
+    [4, 0],
+    [5, 4],
+    [3, 2]
+])
+# d , df = agglomerative_clustering(dataset= hdataset, numCluster= 3, link_type="single")
+# print("=====Return value ======")
+# print(d)
+# print(df)
+
+
+# print(euclidean_distance(np.asarray([[-2.8284, 0],[2.8284, 0]]), np.asarray([-0.7071, -3.5355])))0]]), np.asarray([-0.7071, -3.5355])))
