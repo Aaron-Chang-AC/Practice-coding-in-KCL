75afc7228a50913ccf8c3e4c11ad399e69f86913
rogerchenrc <rogerchen15935717@gmail.com>
2022-05-16 20:36:45 +0100

    update week8

--- PNN/backpropagation.py
+++ PNN/backpropagation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,6 +6,7 @@ import torch.nn.functional as F
 
 
 def my_loss(output, target):
+    # ord = 2 is euclidean distance
     loss = 0.5 * torch.linalg.norm(output-target, dim=1, ord=2)**2
     print(loss)
     return loss
@@ -13,41 +15,42 @@ def tansigmoid(x):
     activation = 2/(1+torch.exp(-2 * x))-1
     return activation
 
+def logarithmic_sigmoid_function(x):
+    activation = 1/(1+torch.exp(-x))
+    return activation
+
+def sgn(x):
+    for i in range(len(x)):
+        if x[i]>= 0:
+            x[i] = 1
+        else:
+            x[i] = -1
+    return x
+
+
+
 class my_network():
     def __init__(self):
-        print("**************PLEASE CHECK YOUR LOSS FUNCTION*****************\n")
-        # weight for layer 0 -> 1
-        # self.w11= torch.tensor([1], dtype=torch.float, requires_grad=True)
-        # self.w21= torch.tensor([2], dtype=torch.float, requires_grad=True)
-        # self.w22= torch.tensor([3], dtype=torch.float, requires_grad=True)
-        # self.w10= torch.tensor([4], dtype=torch.float, requires_grad=True)
-        # # weight for layer 1 -> 2
-        # self.m11= torch.tensor([5], dtype=torch.float, requires_grad=True)
-        # self.m21= torch.tensor([6], dtype=torch.float, requires_grad=True)
-        # self.m22= torch.tensor([7], dtype=torch.float, requires_grad=True)
-        # self.m20= torch.tensor([8], dtype=torch.float, requires_grad=True)
-
-        x = [[-1, 2], [-3, 4], [-3, 4]]  # input_sample
-        t = [[-6, 8], [-2, 4], [-2, 4]]  # target
-
-        self.x_data = torch.tensor(x, dtype=torch.float)
-        self.target_data = torch.tensor(t, dtype=torch.float)
-
+        print("**************PLEASE CHECK YOUR ACTIVATION AND LOSS FUNCTION*****************\n")
         #=====================================================
-        self.w11 = torch.tensor([0.5], dtype=torch.float, requires_grad=True)
-        self.w21 = torch.tensor([0.3], dtype=torch.float, requires_grad=True)
-        self.w22 = torch.tensor([-0.7], dtype=torch.float, requires_grad=True)
-        self.w10 = torch.tensor([0.2], dtype=torch.float, requires_grad=True)
+        self.w11 = torch.tensor([1], dtype=torch.float, requires_grad=True)
+        self.w21 = torch.tensor([1], dtype=torch.float, requires_grad=True)
+        self.w12 = torch.tensor([1], dtype=torch.float, requires_grad=True)
+        self.w22 = torch.tensor([1], dtype=torch.float, requires_grad=True)
+        self.w10 = torch.tensor([0.5], dtype=torch.float, requires_grad=True)
+        self.w20 = torch.tensor([-1.5], dtype=torch.float, requires_grad=True)
         # weight for layer 1 -> 2
-        self.m11 = torch.tensor([0.8], dtype=torch.float, requires_grad=True)
-        self.m12 = torch.tensor([1.6], dtype=torch.float, requires_grad=True)
-        self.m10 = torch.tensor([-0.4], dtype=torch.float, requires_grad=True)
+        self.m11 = torch.tensor([0.7], dtype=torch.float, requires_grad=True)
+        self.m12 = torch.tensor([-0.4], dtype=torch.float, requires_grad=True)
+        self.m10 = torch.tensor([-1], dtype=torch.float, requires_grad=True)
 
-        x = [[0.1, 0.9]]  # input_sample
-        t = [[0.5]]  # target
+        x = [[-1, -1],[-1, 1],[1, -1],[1,1]]  # input_sample
+        t = [[-1, 1, 1, -1]]  # target
 
         self.x_data = torch.tensor(x, dtype=torch.float)
         self.target_data = torch.tensor(t, dtype=torch.float)
+        # for RBF
+        self.center = torch.tensor([[-1, -1],[1, 1]], dtype=torch.float)
         # =========================================================
 
         # initialize activation function
@@ -56,24 +59,19 @@ class my_network():
         self.relu = nn.ReLU()
 
 
+
     def forward(self):
-        y1 = self.w11 * self.x_data[:,0] + self.w10
-        y2 = self.w21 * self.x_data[:,0]+ self.w22 * self.x_data[:,1]
-        y1 = tansigmoid(y1)
-        y2 = tansigmoid(y2)
+        y1 = self.w11 * self.x_data[:,0] + self.w12 * self.x_data[:,1] + self.w10
+        y2 = self.w21 * self.x_data[:,0]+ self.w22 * self.x_data[:,1] + self.w20
+        # print(y1)
+        # print(y2)
         print(y1)
         print(y2)
         z1 = self.m11 * y1 + self.m12 *y2 + self.m10
-        z = tansigmoid(z1)
+        z = sgn(z1)
         return z
 
-    # def forward(self):
-    #     y1 = self.w11 * self.x_data[:,0] + self.w10
-    #     y2 = self.w21 * self.x_data[:,0]+ self.w22 * self.x_data[:,1]
-    #     z1 = self.m11 * y1
-    #     z2 = self.m21 * y1 + self.m22 * y2 + self.m20
-    #     z = torch.stack([z1,z2], dim=0).transpose(0,1)
-    #     return z
+
 
     def seperate_loss(self):
         # Just to check
@@ -94,8 +92,8 @@ class my_network():
         print(f"Loss is: {loss}")
         loss.sum().backward()
         # print(f"Total backpropagation (dJ/dw11): {self.w11.grad}")
-        print(f"Total backpropagation (dJ/dw21): {self.w21.grad}")
-        print(f"Total backpropagation (dJ/dm10): {self.m10.grad}")
+        print(f"Total backpropagation (dJ/dw20): {self.w20.grad}")
+        # print(f"Total backpropagation (dJ/dm10): {self.m10.grad}")
 
         # print(w21.grad)
         # print(w22.grad)
@@ -105,35 +103,35 @@ class my_network():
         # print(m22.grad)
         # print(m20.grad)
 
-    def update_weight(self, learning_rate= 0.1):
+    def update_weight(self, learning_rate= 0.01):
         # update weight
         # self.w11 = self.w11 - learning_rate * self.w11.grad
-        self.w21 = self.w21 - learning_rate * self.w21.grad
-        self.m10 = self.m10 - learning_rate * self.m10.grad
-        print(f"Updated weight of w21: {self.w21}")
-        print(f"Updated weight of m11: {self.m10}")
+        self.w20 = self.w20 - learning_rate * self.w20.grad
+        # self.m10 = self.m10 - learning_rate * self.m10.grad
+        print(f"Updated weight of w20: {self.w20}")
+        # print(f"Updated weight of m11: {self.m10}")
 
 
         # print(f"Updated weight of w11: {self.w11}")
 
     def test(self):
         print("============================TEST RESULT==============================")
-        y1 = self.w11 * self.x_data[:, 0] + self.w10
-        y2 = self.w21 * self.x_data[:, 0] + self.w22 * self.x_data[:, 1]
-        y1 = tansigmoid(y1)
-        y2 = tansigmoid(y2)
+        print(self.center[0])
+        y1 = gaussian(self.x_data[:, 0], self.center, self.center[0])
+        y2 = gaussian(self.x_data[:, 1], self.center, self.center[1])
+        print(y1)
+        print(y2)
         z1 = self.m11 * y1 + self.m12 * y2 + self.m10
-        z = tansigmoid(z1)
-        print(f"The tested result after update is: {z}")
-        print(f"The updated result loss now is: {my_loss(z, self.target_data)}")
+        z = sgn(z1)
         return z
 
-Net = my_network()
-check = False # check for detail dJ1 and dJ2 use True, if combined use False
-if check:
-    Net.seperate_loss()
-else:
-    Net.loss()
-Net.update_weight(learning_rate=0.25)
 
-Net.test()
+Net = my_network()
+# check = False # check for detail dJ1 and dJ2 use True, if combined use False
+# if check:
+#     Net.seperate_loss()
+# else:
+#     Net.loss()
+# Net.update_weight(learning_rate=0.01)
+
+print(Net.test())
